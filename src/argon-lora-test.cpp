b17b14ee650ff83c908ea9b35d66a372b29908c5
Lam Dinh <dinhlam0308@gmail.com>
2023-10-05 15:01:01 -0400

    Sample Program

--- src/argon-lora-test.cpp
+++ src/argon-lora-test.cpp
@@ -11,17 +11,81 @@
  * Date:
  */
 
-// setup() runs once, when the device is first turned on.
+#include <TinyLoRa.h>
+
 void setup();
 void loop();
-#line 9 "e:/Villanova/PhotonProjects/argon-lora-test/src/argon-lora-test.ino"
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+#line 10 "e:/Villanova/PhotonProjects/argon-lora-test/src/argon-lora-test.ino"
+#define LoRa_SS D6
+#define LoRa_SDA D1
+#define LoRa_SCL D2
 
+// Network Session Key (MSB)
+uint8_t NwkSkey[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+// Application Session Key (MSB)
+uint8_t AppSkey[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+// Device Address (MSB)
+uint8_t DevAddr[4] = { 0x00, 0x00, 0x00, 0x00 };
+
+// Data Packet to Send to TTN
+unsigned char loraData[11] = {"hello LoRa"};
+
+// How many times data transfer should occur, in seconds
+const unsigned int sendInterval = 30;
+
+// Pinout for Adafruit Feather 32u4 LoRa
+TinyLoRa lora = TinyLoRa(7, 8, 4);
+
+
+void setup()
+{
+  delay(2000);
+  Serial.begin(9600);
+  while (! Serial);
+  
+  // Initialize pin LED_BUILTIN as an output
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Initialize LoRa
+  Serial.print("Starting LoRa...");
+  // define multi-channel sending
+  lora.setChannel(MULTI);
+  // set datarate
+  lora.setDatarate(SF7BW125);
+  if(!lora.begin())
+  {
+    Serial.println("Failed");
+    Serial.println("Check your radio");
+    while(true);
+  }
+
+  // Optional set transmit power. If not set default is +17 dBm.
+  // Valid options are: -80, 1 to 17, 20 (dBm).
+  // For safe operation in 20dBm: your antenna must be 3:1 VWSR or better
+  // and respect the 1% duty cycle.
+
+  // lora.setPower(17);
+
+  Serial.println("OK");
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+void loop()
+{
+  Serial.println("Sending LoRa Data...");
+  lora.sendData(loraData, sizeof(loraData), lora.frameCounter);
+  // Optionally set the Frame Port (1 to 255)
+  // uint8_t framePort = 1;
+  // lora.sendData(loraData, sizeof(loraData), lora.frameCounter, framePort);
+  Serial.print("Frame Counter: ");Serial.println(lora.frameCounter);
+  lora.frameCounter++;
 
+  // blink LED to indicate packet sent
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  Serial.println("delaying...");
+  delay(sendInterval * 1000);
 }
--- src/argon-lora-test.ino
+++ src/argon-lora-test.ino
@@ -5,14 +5,78 @@
  * Date:
  */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+#include <TinyLoRa.h>
 
+#define LoRa_IRQ D6   // pin connected to LoRa's interrupt pin
+#define LoRa_NSS D1   // pin connected to LoRa's slave select pin
+#define LoRa_RST D2   // pin connected to LoRa's reset pin
+
+// Network Session Key (MSB)
+uint8_t NwkSkey[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+// Application Session Key (MSB)
+uint8_t AppSkey[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+// Device Address (MSB)
+uint8_t DevAddr[4] = { 0x00, 0x00, 0x00, 0x00 };
+
+// Data Packet to Send to TTN
+unsigned char loraData[11] = {"hello LoRa"};
+
+// How many times data transfer should occur, in seconds
+const unsigned int sendInterval = 30;
+
+// Pinout for Adafruit Feather 32u4 LoRa
+TinyLoRa lora = TinyLoRa(7, 8, 4);
+
+
+void setup()
+{
+  delay(2000);
+  Serial.begin(9600);
+  while (! Serial);
+  
+  // Initialize pin LED_BUILTIN as an output
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Initialize LoRa
+  Serial.print("Starting LoRa...");
+  // define multi-channel sending
+  lora.setChannel(MULTI);
+  // set datarate
+  lora.setDatarate(SF7BW125);
+  if(!lora.begin())
+  {
+    Serial.println("Failed");
+    Serial.println("Check your radio");
+    while(true);
+  }
+
+  // Optional set transmit power. If not set default is +17 dBm.
+  // Valid options are: -80, 1 to 17, 20 (dBm).
+  // For safe operation in 20dBm: your antenna must be 3:1 VWSR or better
+  // and respect the 1% duty cycle.
+
+  // lora.setPower(17);
+
+  Serial.println("OK");
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+void loop()
+{
+  Serial.println("Sending LoRa Data...");
+  lora.sendData(loraData, sizeof(loraData), lora.frameCounter);
+  // Optionally set the Frame Port (1 to 255)
+  // uint8_t framePort = 1;
+  // lora.sendData(loraData, sizeof(loraData), lora.frameCounter, framePort);
+  Serial.print("Frame Counter: ");Serial.println(lora.frameCounter);
+  lora.frameCounter++;
 
+  // blink LED to indicate packet sent
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  Serial.println("delaying...");
+  delay(sendInterval * 1000);
 }
